87821dfe994d3a7778b7b70bdcf0713b7d7b314a
tshivam-afk <parabola0098@gmail.com>
2025-07-29 00:54:47 +0530

    Update script.js

--- www/script.js
+++ www/script.js
@@ -1,20 +1,24 @@
-document.addEventListener('deviceready', function() {
-    // Find all links with download attribute
+document.addEventListener('deviceready', function () {
+    var permissions = cordova.plugins.permissions;
+    permissions.requestPermission(permissions.WRITE_EXTERNAL_STORAGE, function () {
+        console.log("Storage permission granted");
+    }, function () {
+        alert("Permission denied");
+    });
+
     document.querySelectorAll('a[download]').forEach(link => {
-        link.addEventListener('click', function(e) {
+        link.addEventListener('click', function (e) {
             e.preventDefault();
+
             var url = this.href;
             var fileName = url.split('/').pop();
-            var filePath = cordova.file.externalDataDirectory + fileName;
+            var filePath = cordova.file.externalRootDirectory + "Download/" + fileName;
 
-            cordova.plugin.http.downloadFile(url, {}, {}, filePath,
-                function(entry) {
-                    alert("Downloaded: " + entry.toURL());
-                },
-                function(err) {
-                    alert("Download failed: " + err.error);
-                }
-            );
+            cordova.plugin.http.downloadFile(url, {}, {}, filePath, function(entry) {
+                alert("Downloaded to: " + entry.toURL());
+            }, function(err) {
+                alert("Download failed: " + JSON.stringify(err));
+            });
         });
     });
 });
